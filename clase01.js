2c29bb3098c7761e1a6c35e4cdb7c1116c7811b2
gusDanMercado <gustavomerk2@gmail.com>
2025-03-18 20:54:53 -0300

    primer commit

---
+++ clase01.js
@@ -0,0 +1,13 @@
+console.log("Hola Mundo!!!!!"); // nos sirve a los programadores para ver si todo esta bien
+
+let edad = 28;
+const nombre = "gustavo";
+let precio = 11.5;
+
+console.log(nombre);
+console.log(edad);
+console.log("Mi nombre es: " + nombre + " y tengo " + edad + " años");
+console.log("Mi nombre es: " + nombre + " y tengo " + edad + " años");
+console.log("Mi nombre es: " + nombre + " y tengo " + edad + " años");
+
+let esMayor = false;
